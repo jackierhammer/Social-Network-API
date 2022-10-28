7fdd79ee2b035066857433937eba681869f29f88
Jackie <jackierhammer@gmail.com>
2022-10-27 18:00:11 -0600

    worked on api routes/router

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,29 @@
 // imports router
 const router = require('express').Router();
+// imports thoughtController
+const {
+    getThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+// in this file, we connect the controllers to their api methods and the router
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:id
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:id/reactions
+router.route('/:id/reactions').post(addReaction);
+
+// /api/thoughts/:id/reactions/:reactionId
+router.route('/:id/reactions/:reactionId').delete(deleteReaction);
 
 // exports router
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,26 @@
 // imports router
 const router = require('express').Router();
+// imports userController
+const {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
+// in this file, we connect the controllers to their api methods and the router
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:id
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+
+// /api/users/:id/friends/:friendId
+router.route('/:id/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 // exports router
 module.exports = router;
